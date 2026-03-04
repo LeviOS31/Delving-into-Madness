4131d6f8a6bf903c870c0b4ac90a8f2e6f5b6e75
Toby Looijkens <138786228+Toby-Looijkens@users.noreply.github.com>
2026-03-04 11:13:57 +0100

    Added dash to player

--- Delving into madness/Assets/Scripts/PlayerController.cs
+++ Delving into madness/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -5,24 +6,32 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject playerModel;
+    [SerializeField] CapsuleCollider playerHitbox;
 
     [Header("Player Stats")]
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float turnSpeed = 5f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float dashCooldown = 0.5f;
+    [SerializeField] private float dashPower = 25;
+    private bool canDash = true;
+
 
     private InputAction movementAction;
     private Rigidbody rigidBody;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool disableMovement;
+
     void Start()
     {
         movementAction = InputSystem.actions.FindAction("Move");
         rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (disableMovement) return;
+
         MovePlayer();
     }
 
@@ -38,5 +47,23 @@ public class PlayerController : MonoBehaviour
 
             playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, Quaternion.LookRotation(normalizedMovement), Time.deltaTime * turnSpeed);
         }
+    }
+
+    private void OnDash()
+    {
+        StartCoroutine(Dash());
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        disableMovement = true;
+        rigidBody.linearVelocity = playerModel.transform.forward * dashPower;
+        playerHitbox.enabled = false;
+        yield return new WaitForSeconds(dashDuration);
+        disableMovement = false;
+        playerHitbox.enabled = true;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
